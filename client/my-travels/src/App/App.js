169ae53587a04d7d8f7d260ebb7861b60620843d
mickmed <mickroth66@gmail.com>
2019-02-27 16:54:57 -0500

    rendered basic map to page

--- client/my-travels/src/App/App.js
+++ client/my-travels/src/App/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Map from '../Map/Map.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-      
-        </header>
+        <Map />
       </div>
     );
   }
--- client/my-travels/src/Map/Map.js
+++ client/my-travels/src/Map/Map.js
@@ -1,3 +1,35 @@
-import React from "react"
-import ReactMapGL from 'react-map-gl';
+  import React from 'react';
+  import ReactMapGL from 'react-map-gl';
+
+  const MAPBOX_TOKEN = 'pk.eyJ1IjoibWlja21lZCIsImEiOiJjanFzdTVtZjEwMnY0NDJzM2g4MXNuNTM0In0.VDbqZxEh0hxXAixRjS9FzA'
+  class Map extends React.Component {
+  
+    state = {
+      viewport: {
+        width: 400,
+        height: 400,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+      }
+    };
+    
+    handleStyleLoad = map => (map.resize())
+    
+    render() {
+      return (
+        <ReactMapGL
+          {...this.state.viewport}
+          onViewportChange={(viewport) => this.setState({viewport})}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{ height: '100%', width: '100%' }}
+        //   onStyleLoad={this.handleStyleLoad}
+        />
+      );
+    }
+  }
+
+export default Map;
+
 
